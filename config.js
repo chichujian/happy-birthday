e035cedd23d61dc8b6d85b23f99028bbb93e2dbc
chichujian <2820144465@qq.com>
2025-05-20 19:48:30 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
-// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
 var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "emmmm",
-        "然然小朋友",
-        "猜猜我要说什么＂,
-        "Happy birthday",
-        "2025.5.21",
+        "送给我",
+        "亲爱的妹妹",
+        "今天是你的生日",
+        "祝你生日快乐"
     ],
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "试着点一下？",
-        play: "来点音乐？",
-        bannar_coming: "氛围"，
-        balloons_flying: "好像还少点东西呢！",
+        turn_on: "开始",
+        play: "音乐",
+        bannar_coming: "颜色",
+        balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
         wish_message: "生日快乐",
